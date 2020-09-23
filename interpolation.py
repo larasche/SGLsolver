06090256a5d47f47184f4800f250d5e8dfc67232
Lara Aschenbeck <larasche@uni-bremen.de>
2020-09-23 19:03:51 +0200

    interpolation.py interpolate the given potential

--- interpolation.py
+++ interpolation.py
@@ -2,14 +2,15 @@
 """
 import fileio
 import numpy as np
-import scipy as sp
+import scipy.interpolate as sp
 
 
 def interpolation_pot(indata):
-    """This function interpolates the potential linear, cubic or polynomial.
+    """This function interpolates the potential linear, cubic or polynomial and
+    writes the result in a file (potential.dat).
 
     Args.:
-        indata: dictionary withe the interpolation points and the x range.
+        indata: dictionary with the interpolation points and the x range.
 
     Returns.:
         int_potential: discretized potential in the x range
@@ -18,14 +19,16 @@ def interpolation_pot(indata):
     y_values = indata["inter_points_y"]
     x_range = np.linspace(indata["xMin"], indata["xMax"], indata["nPoint"])
     if indata["interpolationtype"] == "linear":
-        interpolation = sp.interpolate.interp1d(x_values, y_values,
+        interpolation = sp.interp1d(x_values, y_values,
                                                 kind="linear")
     elif indata["interpolationtype"] == "cspline":
-        interpolation = sp.interpolate.interp1d(x_values, y_values,
-                                                kind="cubic")
+        interpolation = sp.CubicSpline(x_values, y_values,
+                                                bc_type = "natural")
     elif indata["interpolationtype"] == "polynomial":
         interpolation = np.poly1d(np.polyfit(x_values, y_values,
                                              indata["nr_interpolation_points"]
                                              - 1))
     int_potential = interpolation(x_range)
+
+    fileio.write_int_pot(x_range, int_potential, indata["directory"])
     return int_potential
