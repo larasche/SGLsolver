e29c7a5a15399dd53cfcdb920a10e7fc899e55ed
Lara Aschenbeck <larasche@uni-bremen.de>
2020-09-28 14:14:31 +0200

    Plots the potential

--- visualisation.py
+++ visualisation.py
@@ -1,2 +1,30 @@
-# -*- coding: utf-8 -*-
+"""Visualisation of the engergies, the potential and the wave function."""
+import numpy as np
+import matplotlib.pyplot as plt
+import fileio
 
+
+def graph(indata):
+    """Creates a plot of wavefunctions for a potential,
+        depending on different energies.
+        Args:
+            energies: eigenenergies
+            discrpot: potentials
+            wafef: wafefunctions
+            xrange: x values
+            xmin: min. value of the x-axis
+            xmax: max. value of the x-axis
+            ymin: min. value of the y-axis
+            ymax: max. value of the y-axis
+        """
+    xrange, potential = fileio.read_int_pot(indata["directory"])
+    plt.plot(xrange, potential)
+ #   plt.plot(xrange, wavef + energies)
+  #  plt.ylim(ymin, ymax)
+   # plt.xlim(xmin, xmax)
+
+# horizontal line
+    #for ii in energies:
+     #   plt.axhline(ii, -2, 2)
+
+    return
